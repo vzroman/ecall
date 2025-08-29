1e28f4adbbf3b93d2a2ded16b9396396ddd7fe4e
Roman VZ <vzroman@gmail.com>
2025-08-29 10:33:39 +0500

    save

--- test/load/ecall_scale_SUITE.erl
+++ test/load/ecall_scale_SUITE.erl
@@ -90,7 +90,8 @@ build_image(Config)->
     "FROM vzroman/erlang_otp:v27.2.3",
     "EXPOSE 4445\n"
     "ENV SRC=/opt/ecall",
-    "ENV ERL_FLAGS=\"-args_file config/vm.args -config config/sys.config -user peer\"",
+%%    "ENV ERL_FLAGS=\"-args_file config/vm.args -config config/sys.config\"",
+%%    "ENV ERL_FLAGS=\"-args_file config/vm.args -config config/sys.config -user peer\"",
     "RUN mkdir $SRC",
     "COPY ./src $SRC/",
     "WORKDIR $SRC",
@@ -120,12 +121,22 @@ start_node( Host )->
     longnames => true,
     connection => standard_io,
     post_process_args => fun to_rebar_args/1,
-    exec => {Docker, ["run", "-h", Host, "-i", "ecall"]}}),
+    exec => {Docker, [
+      "run",
+      "-e","ERL_FLAGS=\"-name","ecall@"++Host,
+      "-name","ecall@"++Host,
+      "-user","peer",
+      "-args_file","config/vm.args",
+      "-config","config/sys.config\"",
+%%      "-e","ERL_FLAGS=-user peer",
+      "-h", Host,
+      "-i", "ecall"
+    ]}}),
 
   {Node, Peer}.
 
-to_rebar_args(["-name", Name|Rest])->
-  ["--name", Name | to_rebar_args(Rest)];
+to_rebar_args(["-name", _Name|Rest])->
+  to_rebar_args(Rest);
 to_rebar_args(["-user", _User|Rest])->
   to_rebar_args(Rest);
 to_rebar_args([Option|Rest])->
