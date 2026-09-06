c866b6727565aef0af6476e25b3b803487cd5e04
roman <vzroman@gmail.com>
2026-09-06 20:45:26 +0500

    call receive optimization

--- src/ecall_receive.erl
+++ src/ecall_receive.erl
@@ -10,8 +10,7 @@
   start_link/0
 ]).
 
--record(state,{ ref2pid = #{}, pid2ref = #{}}).
--record(p2r,{ ref, pid, type}).
+-record(state,{}).
 
 %%=================================================================
 %% OTP API
@@ -63,20 +62,6 @@ worker_loop( State )->
     {batch, Batch}->
       State1 = handle_batch(Batch, State),
       worker_loop( State1 );
-    {'DOWN', _, process, PID, Reason}->
-      case maps:take(PID, State#state.pid2ref) of
-        { #p2r{ref = Ref, pid = ClientPID }, P2R }->
-          if
-            Reason =/= normal->
-              catch ecall_connection:send(ClientPID, {'DOWN', Ref, Reason});
-            true ->
-              ignore
-          end,
-          worker_loop( State#state{ pid2ref = P2R, ref2pid = maps:remove( Ref, State#state.ref2pid )} );
-        _->
-          % Who was that?
-          worker_loop( State )
-      end;
     _Unexpected->
       worker_loop( State )
   end.
@@ -90,16 +75,16 @@ handle_batch([{send, To, Message}| Rest], State)->
 handle_batch([{cast, Module, Function, Args}| Rest], State)->
   spawn(Module, Function, Args ),
   handle_batch( Rest, State);
-handle_batch([{call, Ref, ClientPID,  Module, Function, Args}| Rest], #state{ref2pid = R2P, pid2ref = P2R} = State)->
-  {PID,_} =
-    spawn_monitor(fun()->
+handle_batch([{call, Ref, ClientPID,  Module, Function, Args}| Rest], State)->
+  spawn(fun()->
+    try
       Result = apply(Module, Function, Args),
       ecall_connection:send( ClientPID, {Ref, Result} )
-    end),
-  State1 = State#state{
-    ref2pid = R2P#{ Ref => PID},
-    pid2ref = P2R#{ PID => #p2r{ ref = Ref, pid = ClientPID, type = call }}
-  },
-  handle_batch( Rest, State1);
+    catch
+      _:Reason->
+        ecall_connection:send(ClientPID, {'DOWN', Ref, Reason})
+    end
+  end),
+  handle_batch( Rest, State);
 handle_batch([], State)->
   State.
